2e63bb6a9b965f43582e3a0169645339810b9458
kopokopok <michaelzhou7@gmail.com>
2021-04-05 19:02:17 -0400

    test.py

--- test.py
+++ test.py
@@ -7,35 +7,7 @@
 # import math
 
 def main():
-    s, stack, num = input(), [], []
-
-
-    for ch in s:
-        if ch == '(':
-            stack.append('(')
-        elif stack[-1] == '(':
-            stack.append('')
-            if ch >= 'a' and ch <= 'z':
-                stack[-1] += ch
-        elif ch >= 'a' and ch <= 'z':
-            stack[-1] += ch
-        elif ch >= '0' and ch <= '9':
-            num.append(ch)
-        elif ch == '}':
-            s1 = stack.pop()
-            s1 = s1 * int(''.join(num))
-            if stack[-1] == '(':
-                stack.pop()
-            if stack and stack[-1] != '(':
-                stack[-1] += s1
-            else:
-                stack.append(s1)
-            num.clear()
-
-    print(stack.pop())
-
-# ((((ab){2}){3}){5}){2}c
-# (((){2}){3}){4}
+    pass
 
 if __name__ == "__main__":
     main()
