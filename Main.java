b577921aa15347ea7f5b8dd48a354407f309f826
kopokopok <michaelzhou7@gmail.com>
2021-05-19 20:53:39 -0400

    USACO doesn't like python apparently

--- Main.java
+++ Main.java
@@ -1,64 +1,68 @@
 import java.util.*;
 
 public class Main {
-    private int k;
-    private int n;
-    private String[] ppl;
-    private String[][] arr;
+    static final int[] imove = {-1, 0, 0, 1};
+    static final int[] jmove = {0, -1, 1, 0};
 
-    public Main(int k, int n, String[] ppl, String[][] arr) {
-        this.k = k;
+    private int n;
+    private int m;
+    private String[] arr;
+
+    public Main(int n, int m, String[] arr) {
         this.n = n;
-        this.ppl = ppl;
+        this.m = m;
         this.arr = arr;
     }
 
-    public void acowdemiaII() {
-        Map<String, Integer> freq = new HashMap<String, Integer>();
+    public boolean inBounds(int x, int y) {
+        if(x >= 0 && y >= 0 && x < n && y < m)
+            return true;
+        return false;
+    }
 
-        for(int i=0; i<k; i++) {
-            int cnt = 0;
-            for(int j=1; j<n; j++) {
-                // System.out.println("aa".compareTo("ab"));
-                if(arr[i][j-1].compareTo(arr[i][j]) > 0) {
-                    cnt++;
+    public int acowdemiaIII() {
+        Set<Map.Entry> friends = new HashSet<>();
+        // Map.Entry<Integer, Integer> pair = new AbstractMap.SimpleImmutableEntry<>(0, 1);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(arr[i].charAt(j) == 'G') {
+                    List<Map.Entry> cows = new ArrayList<>();
+                    for(int k = 0; k < 4; k++) {
+                        int ii = i + imove[k];
+                        int jj = j + jmove[k];
+                        if(this.inBounds(ii, jj) && arr[ii].charAt(jj) == 'C')
+                            cows.add(new AbstractMap.SimpleImmutableEntry<>(ii, jj));
+                    }
+                    for(int a = 0; a < cows.size(); a++) {
+                        boolean tmp = true;
+                        for(int b = a+1; b < cows.size(); b++) {
+                            Map.Entry<Map.Entry, Map.Entry> pair = new AbstractMap.SimpleImmutableEntry<>(cows.get(a), cows.get(b));
+                            if(!friends.contains(pair)) {
+                                friends.add(pair);
+                                friends.add(new AbstractMap.SimpleImmutableEntry<>(cows.get(b), cows.get(a)));
+                                tmp = true;
+                                break;
+                            }
+                        }
+                        if(tmp)
+                            break;
+                    }
                 }
-                freq.put(arr[i][j], freq.getOrDefault(arr[i][j], 0) + cnt);
             }
         }
 
-        for(int i=0; i<n; i++) {
-            String[] res = new String[n];
-            int a = freq.getOrDefault(ppl[i], 0);
-            for(int j=0; j<n; j++) {
-                if(i == j) {
-                    res[j] = "B";
-                    continue;
-                }
-                int b = freq.getOrDefault(ppl[j], 0);
-                if(a > b)
-                    res[j] = "1";
-                else if(a < b)
-                    res[j] = "0";
-                else
-                    res[j] = "?";
-            }
-            System.out.println(String.join("", res));
-        }
+        return friends.size() / 2;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int k = sc.nextInt(); int n = sc.nextInt();
-        String[] ppl = new String[n];
-        for(int i=0; i<n; i++)
-            ppl[i] = sc.next();
-        String[][] arr = new String[k][n];
-        for(int i=0; i<k; i++)
-            for(int j=0; j<n; j++)
-                arr[i][j] = sc.next();
+        int n = sc.nextInt(); int m = sc.nextInt();
+        String[] arr = new String[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.next();
 
-        Main s = new Main(k, n, ppl, arr);
-        s.acowdemiaII();
+        Main s = new Main(n, m, arr);
+        System.out.println(s.acowdemiaIII());
     }
 }
