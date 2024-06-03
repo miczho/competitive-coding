3480f012375a54b5632b9371fb324c1fbdeeae23
miczho <michaelzhou7@gmail.com>
2024-06-03 00:01:16 -0400

    added desc

--- src/leetcode-3171.py
+++ src/leetcode-3171.py
@@ -1,6 +1,11 @@
 """
 Was not able to figure it out on my own. Thought of using DP and while it is possible, it is better solved with sliding window.
 
+The key is to realize that bitwise result can NEVER increase when you add numbers to the AND chain.
+Similarly, the bitwise result can NEVER decrease when you remove numbers from the AND chaim.
+
+This requires understanding of how the AND operation works; it only takes one '0' to set the result bit to '0'.
+
 Time Complexity:
 O((n + n) * 32)
 = O(32 * 2n)
