8f55b5cdbe8260950010046c355f3b9d3d9a331a
Mike Zhou <michaelzhou7@gmail.com>
2024-07-06 04:40:15 -0400

    src/neetcode_wordBreak.py

--- src/neetcode_wordBreak.py
+++ src/neetcode_wordBreak.py
@@ -46,9 +46,55 @@ class Solution:
 
 
 
+"""
+Attempt 2: Can you reduce the time and space complexity?
+
+Time Complexity:
+O(N^2)
+
+Space Complexity:
+O(N + M), where N = length of 's' and M = length of 'wordDict'
+"""
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDictSet = set(wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] == True and s[j:i] in wordDictSet:
+                    dp[i] = True
+                    break
+        
+        return dp[n]
+
 
 """
-Attempt 2: Can you lower the time and space complexity?
+Attempt 3: Can you do the top-down approach with the same time and space complex?
 
 #revisit
 """
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDictSet = set(wordDict)
+        n = len(s)
+        memo = [None for _ in range(n)]
+
+        def dfs(start):
+            if start == n:
+                return True
+
+            if memo[start] == None:
+                memo[start] = False
+
+                for end in range(start + 1, n + 1):
+                    if s[start:end] in wordDictSet and dfs(end):
+                        memo[start] = True
+
+            return memo[start]
+        
+        return dfs(0)
