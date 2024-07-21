ad9604a3d62f8a68e1d6f2d47434a6636d2e163a
Michael Zhou <michaelzhou7@gmail.com>
2024-07-21 14:04:45 -0400

    update leetcode-152

--- src/leetcode_152.py
+++ src/leetcode_152.py
@@ -23,3 +23,36 @@ class Solution(object):
             self.dp[(pos, 1)] = max(nums[pos], nums[pos] * self.dfs(pos+1, nums)[0], nums[pos] * self.dfs(pos+1, nums)[1])
 
         return (self.dp[(pos, 0)], self.dp[(pos, 1)])
+
+
+
+"""
+Attempt 2: reducing space complexity
+
+Time Complexity:
+O(N)
+
+Space Complexity:
+O(1)
+"""
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        currMin = 1
+        currMax = 1
+        result = float("-inf")
+
+        for num in nums:
+            newMin = min(currMin * num, currMax * num, num)
+            newMax = max(currMin * num, currMax * num, num)
+
+            currMin = newMin
+            currMax = newMax
+
+            result = max(result, currMax)
+
+        return result
