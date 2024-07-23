63fa6bd109bd5174957db880c5b4458e9d90159a
Michael Zhou <michaelzhou7@gmail.com>
2024-07-23 03:31:26 -0400

    src/neetcode_groupAnagrams.py

---
+++ src/neetcode_groupAnagrams.py
@@ -0,0 +1,31 @@
+"""
+Asked in TikTok round 2 on 7/22/24
+
+Time Complexity:
+O(N*K) where N = number of words and K = number of characters
+
+Space Complexity:
+O(N) where N = number of words
+
+https://neetcode.io/problems/anagram-groups
+https://leetcode.com/problems/group-anagrams
+
+#blind75 #2024
+"""
+
+class Solution(object):
+    def groupAnagrams(self, words):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        result = defaultdict(lambda: [])
+
+        for word in words:
+            key = [0 for _ in range(26)]
+            for ch in word:
+                idx = ord(ch) - ord("a")
+                key[idx] += 1
+            result[tuple(key)].append(word)
+
+        return result.values()
