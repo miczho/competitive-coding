e4ea5c55c32eeb6a7761620e6e211e8ecec75531
Mike Zhou <michaelzhou7@gmail.com>
2024-09-09 02:28:46 -0400

    topSort practice

---
+++ src/leetcode_310.py
@@ -0,0 +1,40 @@
+"""
+https://leetcode.com/problems/minimum-height-trees
+
+#favorite #topSort #2024
+"""
+
+class Solution:
+    def findMinHeightTrees(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        children = [set() for _ in range(n)]
+        queue = []
+
+        for u, v in edges:
+            children[u].add(v)
+            children[v].add(u)
+
+        for node in range(n):
+            if len(children[node]) <= 1:
+                queue.append(node)
+
+        while True:
+            nextQueue = []
+
+            for node in queue:
+                for child in children[node]:
+                    children[child].remove(node)
+                    if len(children[child]) == 1:
+                        nextQueue.append(child)
+                children[node].clear()
+
+            if len(nextQueue) != 0:
+                queue = nextQueue
+            else:
+                break
+        
+        return queue
