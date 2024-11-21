df1d0c4bb42b7898ee127ba02fd1d164d190a557
Michael Zhou <michaelzhou7@gmail.com>
2024-11-21 03:23:29 -0800

    revisiting problem

--- src/leetcode_1169.py
+++ src/leetcode_1169.py
@@ -1,9 +1,7 @@
 """
-There should be an O(nlogn) solution to this at least, maybe even O(n)
-
 https://leetcode.com/problems/invalid-transactions/
 
-#2024 #revisit
+#2024
 """
 
 class Solution:
@@ -42,3 +40,41 @@ class Solution:
                     break
 
         return result
+
+
+    def invalidTransactions2(self, transactions: list[str]) -> list[str]:
+        """
+        Chàtġpţ's proposed solution
+        """
+
+        parsed_transactions = [
+            (
+                t.split(",")[0],
+                int(t.split(",")[1]),
+                int(t.split(",")[2]),
+                t.split(",")[3],
+                i,
+            )
+            for i, t in enumerate(transactions)
+        ]
+
+        parsed_transactions.sort(key=lambda x: x[1])
+
+        invalid_set = set()
+
+        for i, (name, time, amount, location, idx) in enumerate(parsed_transactions):
+            if amount > 1000:
+                invalid_set.add(idx)
+
+            for j in range(i + 1, len(parsed_transactions)):
+                if parsed_transactions[j][1] - time > 60:
+                    break
+
+                if (
+                    parsed_transactions[j][0] == name
+                    and parsed_transactions[j][3] != location
+                ):
+                    invalid_set.add(idx)
+                    invalid_set.add(parsed_transactions[j][4])
+
+        return [transactions[i] for i in invalid_set]
