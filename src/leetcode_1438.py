71403fc9515a8e3027f9e04664082ab3ca7167b3
Mike Zhou <michaelzhou7@gmail.com>
2025-01-06 12:26:55 -0500

    formatting

--- src/leetcode_1438.py
+++ src/leetcode_1438.py
@@ -2,7 +2,7 @@
 Was not able to solve problem without looking at solution.
 Both the heap solution and the monotonic queue solution are tricky.
 
-https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit
+https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
 
 #2024 #favorite #monotonicQueue #deque #incomplete #revisit
 """
